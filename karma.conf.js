fab5e0c3dd5bc1e7c3b63f9fed20d56c4c465bf5
jagadeeshg0801 <jagadeeshg0801@gmail.com>
2022-02-26 21:53:16 +0530

    project name changes done

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-ngrx-store-sample'),
+      dir: require('path').join(__dirname, './coverage/angular-lib-example'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-ngrx-store-sample'`, () => {
+  it(`should have as title 'angular-lib-example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ngrx-store-sample');
+    expect(app.title).toEqual('angular-lib-example');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-ngrx-store-sample app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-lib-example app is running!');
   });
 });
